ae3bac3a73962f8c9f346e99963a77a7fdfd55fd
Florian Heck <florian.heck@hotmail.de>
2016-12-27 23:11:05 +0100

    parse commits from issues

--- src/de/uni_passau/fim/heck/githubinterface/IssueDataPostprocessor.java
+++ src/de/uni_passau/fim/heck/githubinterface/IssueDataPostprocessor.java
@@ -1,20 +1,26 @@
 package de.uni_passau.fim.heck.githubinterface;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import de.uni_passau.fim.heck.githubinterface.datadefinitions.CommentData;
 import de.uni_passau.fim.heck.githubinterface.datadefinitions.EventData;
 import de.uni_passau.fim.heck.githubinterface.datadefinitions.IssueData;
+import de.uni_passau.fim.seibt.gitwrapper.repo.Commit;
 import io.gsonfire.PostProcessor;
 
 public class IssueDataPostprocessor implements PostProcessor<IssueData> {
 
     private final GitHubRepository repo;
 
-    public IssueDataPostprocessor(GitHubRepository repo) {
+    IssueDataPostprocessor(GitHubRepository repo) {
         this.repo = repo;
     }
 
@@ -26,6 +32,47 @@ public class IssueDataPostprocessor implements PostProcessor<IssueData> {
 
         comments.ifPresent(list -> list.forEach(result::addComment));
         events.ifPresent(list -> list.forEach(result::addEvent));
+
+        parseCommits(result).forEach(result::addRelatedCommit);
+    }
+
+    /**
+     * Parses commits from issue body, comment bodies and reference events.
+     *
+     * @param issue
+     *         the issue data
+     * @return a list of all referenced commits
+     */
+    private List<Commit> parseCommits(IssueData issue) {
+        Stream<String> commentCommits = issue.getCommentsList().stream().map(comment -> comment.body)
+                .flatMap(body -> extractSHA1s(body).stream());
+
+        Stream<String> referencedCommits = issue.getEventsList().stream()
+                .filter(eventData -> eventData instanceof EventData.ReferencedEventData)
+                .map(eventData -> ((EventData.ReferencedEventData) eventData).commit_id);
+
+
+        return Stream.concat(Stream.concat(commentCommits, referencedCommits), extractSHA1s(issue.body).stream())
+                .map(repo::getCommit)
+
+                // filter out false positive matches on normal words (and other errors)
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .distinct()
+                .collect(Collectors.toList());
+    }
+
+    private List<String> extractSHA1s(String text) {
+        Pattern sha1Pattern = Pattern.compile("([0-9a-f]){5,40}");
+        Matcher matcher = sha1Pattern.matcher(text);
+
+        List<String> sha1s = new ArrayList<>();
+
+        while (matcher.find()) {
+            sha1s.add(matcher.group(1));
+        }
+
+        return sha1s;
     }
 
     @Override
