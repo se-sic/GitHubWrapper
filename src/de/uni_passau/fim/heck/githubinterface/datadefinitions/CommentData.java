bce3a2ee4e5bb98ed0b8f481a20f6dfa51ceb9bb
Florian Heck <florian.heck@hotmail.de>
2016-12-27 23:10:37 +0100

    extended data definitions

--- src/de/uni_passau/fim/heck/githubinterface/datadefinitions/CommentData.java
+++ src/de/uni_passau/fim/heck/githubinterface/datadefinitions/CommentData.java
@@ -3,6 +3,8 @@ package de.uni_passau.fim.heck.githubinterface.datadefinitions;
 import java.util.Date;
 
 public class CommentData {
+
     public UserData user;
     public Date created_at;
+    public String body;
 }
--- src/de/uni_passau/fim/heck/githubinterface/datadefinitions/IssueData.java
+++ src/de/uni_passau/fim/heck/githubinterface/datadefinitions/IssueData.java
@@ -5,6 +5,9 @@ import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
+import com.google.gson.annotations.SerializedName;
+import de.uni_passau.fim.seibt.gitwrapper.repo.Commit;
+
 public class IssueData {
 
     public int number;
@@ -13,9 +16,14 @@ public class IssueData {
     public Date created_at;
     public Date closed_at;
     public boolean isPullRequest;
+    public String body;
+
+    @SerializedName(value = "url", alternate = {"html_url"})
+    public String url;
 
     private List<CommentData> commentsList = new ArrayList<>();
     private List<EventData> eventsList = new ArrayList<>();
+    private List<Commit> relatedCommits = new ArrayList<>();
 
     public void addComment(CommentData comment) {
         commentsList.add(comment);
@@ -25,11 +33,19 @@ public class IssueData {
         eventsList.add(event);
     }
 
+    public void addRelatedCommit(Commit commit) {
+        relatedCommits.add(commit);
+    }
+
     public List<CommentData> getCommentsList() {
         return Collections.unmodifiableList(commentsList);
     }
 
     public List<EventData> getEventsList() {
         return Collections.unmodifiableList(eventsList);
+    }
+
+    public List<Commit> getRelatedCommits() {
+        return Collections.unmodifiableList(relatedCommits);
     }
 }
