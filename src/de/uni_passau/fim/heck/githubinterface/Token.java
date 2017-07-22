4d339e7e32eaa935c205d5bdd5ecded387045500
Gustavo Vale <GustavoVale@users.noreply.github.com>
2017-07-22 12:09:44 +0200

    Update Token.java

--- src/de/uni_passau/fim/heck/githubinterface/Token.java
+++ src/de/uni_passau/fim/heck/githubinterface/Token.java
@@ -1,9 +1,25 @@
 package de.uni_passau.fim.heck.githubinterface;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+
+import org.apache.http.Header;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClients;
 
 public class Token {
 
+	private static final Logger LOG = Logger.getLogger(GitHubRepository.class.getCanonicalName());
+	
     private final String token;
     private int calls;
     private Date resetTime;
@@ -24,18 +40,49 @@ public class Token {
         return token;
     }
 
-    void update(int calls, Date resetTime) {
+    synchronized void update(int calls, Date resetTime) {
         this.calls = calls;
         this.resetTime = resetTime;
     }
 
     public boolean isValid() {
-       return calls > 0 || new Date().after(resetTime);
+       return calls > 10;
     }
+    
+    public boolean isUpdatedValid() {
+        startingToken();
+    	return calls > 10;
+     }
 
     Date getResetTime() {
         return resetTime;
     }
+    
+    public synchronized boolean startingToken() {
+
+		final HttpClient hc = HttpClients.createDefault();
+
+		HttpResponse resp;
+		try {
+			resp = hc.execute(new HttpGet("https://api.github.com/rate_limit?access_token=" + token));
+
+			Map<String, List<String>> headers = Arrays.stream(resp.getAllHeaders()).collect(Collectors
+					.toMap(Header::getName, h -> new ArrayList<>(Collections.singletonList(h.getValue())), (a, b) -> {
+						a.addAll(b);
+						return a;
+					}));
+
+			int rateLimitRemaining = Integer
+					.parseInt(headers.getOrDefault("X-RateLimit-Remaining", Collections.singletonList("")).get(0));
+			Date rateLimitReset = new Date(Long.parseLong(headers.get("X-RateLimit-Reset").get(0)) * 1000);
+			update(rateLimitRemaining, rateLimitReset);
+			
+			resp.getEntity().getContent().close();
+		} catch (IOException e) {
+			LOG.warning("Could not get data from GitHub.");
+		}
+		return true;
+	}
 
     @Override
     public String toString() {
